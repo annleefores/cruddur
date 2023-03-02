02900aa9ecb809a015adf49b25e350188dfe31bc
Annlee Fores <annleefores@gmail.com>
2023-03-02 05:47:03 +0000

    feat: rollbar

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -34,6 +34,11 @@ from services.show_activity import *
 # import logging
 # from time import strftime
 
+# ----------Rollbar-----------
+import rollbar
+import rollbar.contrib.flask
+from flask import got_request_exception
+
 #--------Honeycomb----------
 # Initialize tracing and an exporter that can send data to Honeycomb
 provider = TracerProvider()
@@ -43,9 +48,26 @@ trace.set_tracer_provider(provider)
 tracer = trace.get_tracer(__name__)
 #-----------------------------
 
-
-
 app = Flask(__name__)
+
+# rollbar - init
+rollbar_access_token = os.getenv('ROLLBAR_ACCESS_TOKEN')
+@app.before_first_request
+def init_rollbar():
+    """init rollbar module"""
+    rollbar.init(
+        # access token
+        rollbar_access_token,
+        # environment name
+        'production',
+        # server root directory, makes tracebacks prettier
+        root=os.path.dirname(os.path.realpath(__file__)),
+        # flask already sets up logging
+        allow_logging_basic_config=False)
+
+    # send exceptions from `app` to rollbar, using flask's signal system.
+    got_request_exception.connect(rollbar.contrib.flask.report_exception, app)
+
 
 # # Configuring Logger to Use CloudWatch
 # LOGGER = logging.getLogger(__name__)
@@ -80,11 +102,18 @@ cors = CORS(
     methods="OPTIONS,GET,HEAD,POST",
 )
 
+## ---cloudwatch-watchtower----
 # @app.after_request
 # def after_request(response):
 #     timestamp = strftime('[%Y-%b-%d %H:%M]')
 #     LOGGER.error('%s %s %s %s %s %s', timestamp, request.remote_addr, request.method, request.scheme, request.full_path, response.status)
 #     return response
+
+#----- rollbar---------------
+@app.route('/rollbar/test')
+def rollbar_test():
+    rollbar.report_message('Hello World!', 'warning')
+    return "Hello World!"
 
 @app.route("/api/message_groups", methods=["GET"])
 def data_message_groups():
