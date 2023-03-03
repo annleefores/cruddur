9c1f4743668f928da7baa1d0d1e33b470c0082dd
AnnleeFores <annleefores@gmail.com>
2023-03-03 11:53:27 +0530

    Frontend Honeycomb - OTEL - OTEL Collector

--- frontend-react-js/src/index.js
+++ frontend-react-js/src/index.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import "./components/tracing.js";
 
-const el_main = document.getElementsByTagName('main')[0];
+const el_main = document.getElementsByTagName("main")[0];
 const root = ReactDOM.createRoot(el_main);
 root.render(
   <React.StrictMode>
