dd815b14e22643019e75ac3cd2c688e319d1c232
AnnleeFores <annleefores@gmail.com>
2023-03-03 13:18:39 +0530

    otpl collector debug

--- frontend-react-js/src/pages/HomeFeedPage.js
+++ frontend-react-js/src/pages/HomeFeedPage.js
@@ -1,14 +1,19 @@
-import './HomeFeedPage.css';
+import "./HomeFeedPage.css";
 import React from "react";
 
-import DesktopNavigation  from '../components/DesktopNavigation';
-import DesktopSidebar     from '../components/DesktopSidebar';
-import ActivityFeed from '../components/ActivityFeed';
-import ActivityForm from '../components/ActivityForm';
-import ReplyForm from '../components/ReplyForm';
+import DesktopNavigation from "../components/DesktopNavigation";
+import DesktopSidebar from "../components/DesktopSidebar";
+import ActivityFeed from "../components/ActivityFeed";
+import ActivityForm from "../components/ActivityForm";
+import ReplyForm from "../components/ReplyForm";
 
 // [TODO] Authenication
-import Cookies from 'js-cookie'
+import Cookies from "js-cookie";
+
+// Honeycomb OTEL
+import { trace, context } from "@opentelemetry/api";
+
+const tracer = trace.getTracer();
 
 export default function HomeFeedPage() {
   const [activities, setActivities] = React.useState([]);
@@ -18,17 +23,40 @@ export default function HomeFeedPage() {
   const [user, setUser] = React.useState(null);
   const dataFetchedRef = React.useRef(false);
 
+  //-----Honeycomb--------
+
+  const rootSpan = tracer.startActiveSpan("document_load", (span) => {
+    //start span when navigating to page
+    span.setAttribute("pageUrlwindow", window.location.href);
+    window.onload = (event) => {
+      // ... do loading things
+      // ... attach timing information
+      span.end(); //once page is loaded, end the span
+    };
+
+    // button.clicked = (event) => {
+    //   tracer.startActiveSpan("button_clicked", (btnSpan) => {
+    //     // Add your attributes to describe the button clicked here
+    //     btnSpan.setAttribute("some.attribute", "some.value");
+
+    //     btnSpan.end();
+    //   });
+    // };
+  });
+
+  //--------------------
+
   const loadData = async () => {
     try {
-      const backend_url = `${process.env.REACT_APP_BACKEND_URL}/api/activities/home`
+      const backend_url = `${process.env.REACT_APP_BACKEND_URL}/api/activities/home`;
       const res = await fetch(backend_url, {
-        method: "GET"
+        method: "GET",
       });
       let resJson = await res.json();
       if (res.status === 200) {
-        setActivities(resJson)
+        setActivities(resJson);
       } else {
-        console.log(res)
+        console.log(res);
       }
     } catch (err) {
       console.log(err);
@@ -36,46 +64,46 @@ export default function HomeFeedPage() {
   };
 
   const checkAuth = async () => {
-    console.log('checkAuth')
+    console.log("checkAuth");
     // [TODO] Authenication
-    if (Cookies.get('user.logged_in')) {
+    if (Cookies.get("user.logged_in")) {
       setUser({
-        display_name: Cookies.get('user.name'),
-        handle: Cookies.get('user.username')
-      })
+        display_name: Cookies.get("user.name"),
+        handle: Cookies.get("user.username"),
+      });
     }
   };
 
-  React.useEffect(()=>{
+  React.useEffect(() => {
     //prevents double call
     if (dataFetchedRef.current) return;
     dataFetchedRef.current = true;
 
     loadData();
     checkAuth();
-  }, [])
+  }, []);
 
   return (
     <article>
-      <DesktopNavigation user={user} active={'home'} setPopped={setPopped} />
-      <div className='content'>
-        <ActivityForm  
+      <DesktopNavigation user={user} active={"home"} setPopped={setPopped} />
+      <div className="content">
+        <ActivityForm
           popped={popped}
-          setPopped={setPopped} 
-          setActivities={setActivities} 
+          setPopped={setPopped}
+          setActivities={setActivities}
         />
-        <ReplyForm 
-          activity={replyActivity} 
-          popped={poppedReply} 
-          setPopped={setPoppedReply} 
-          setActivities={setActivities} 
-          activities={activities} 
+        <ReplyForm
+          activity={replyActivity}
+          popped={poppedReply}
+          setPopped={setPoppedReply}
+          setActivities={setActivities}
+          activities={activities}
         />
-        <ActivityFeed 
-          title="Home" 
-          setReplyActivity={setReplyActivity} 
-          setPopped={setPoppedReply} 
-          activities={activities} 
+        <ActivityFeed
+          title="Home"
+          setReplyActivity={setReplyActivity}
+          setPopped={setPoppedReply}
+          activities={activities}
         />
       </div>
       <DesktopSidebar user={user} />
