cd4d01777ccfe1732a01745ca94a4b865aa30a7c
annleefores <annleefores@gmail.com>
2023-03-24 15:04:01 +0530

    feat: implement backend for ddb and messages

---
+++ backend-flask/db/sql/users/short.sql
@@ -0,0 +1,6 @@
+SELECT
+    users.uuid,
+    users.handle,
+    users.display_name
+FROM public.users
+WHERE users.handle = %(handle)s
--- frontend-react-js/src/App.js
+++ frontend-react-js/src/App.js
@@ -8,6 +8,7 @@ import SigninPage from "./pages/SigninPage";
 import RecoverPage from "./pages/RecoverPage";
 import MessageGroupsPage from "./pages/MessageGroupsPage";
 import MessageGroupPage from "./pages/MessageGroupPage";
+import MessageGroupNewPage from "./pages/MessageGroupNewPage";
 import ConfirmationPage from "./pages/ConfirmationPage";
 import React from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
@@ -52,6 +53,10 @@ const router = createBrowserRouter([
   {
     path: "/messages",
     element: <MessageGroupsPage />,
+  },
+  {
+    path: "/messages/new/:handle",
+    element: <MessageGroupNewPage />,
   },
   {
     path: "/messages/:message_group_uuid",
