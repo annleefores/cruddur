24a86ba7bfd0dfc65b6eebc148162cd87a6aaee6
annleefores <annleefores@gmail.com>
2023-04-11 10:17:36 +0530

    cdk for serverless avatar

---
+++ thumbing-serverless-cdk/lib/thumbing-serverless-cdk-stack.ts
@@ -0,0 +1,21 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as s3 from 'aws-cdk-lib/aws-s3'
+
+export class ThumbingServerlessCdkStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+    const bucketName: string = process.env.THUMBING_BUCKET_NAME as string;
+    const bucket = this.createBucket(bucketName);
+  }
+
+  createBucket(bucketName: string): s3.IBucket {
+    const bucket = new s3.Bucket(this, 'ThumbingBucket', {
+      bucketName: bucketName,
+      removalPolicy: cdk.RemovalPolicy.DESTROY
+    });
+    return bucket;
+  }
+}
