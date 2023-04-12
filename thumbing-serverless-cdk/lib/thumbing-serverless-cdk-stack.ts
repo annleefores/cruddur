de0b5ce236932b1b5b30b1154b9944b71b0e523b
annleefores <annleefores@gmail.com>
2023-04-12 18:39:43 +0530

    serving avatars via cloudfront and restructuring arch

--- thumbing-serverless-cdk/lib/thumbing-serverless-cdk-stack.ts
+++ thumbing-serverless-cdk/lib/thumbing-serverless-cdk-stack.ts
@@ -19,39 +19,42 @@ export class ThumbingServerlessCdkStack extends cdk.Stack {
     super(scope, id, props);
 
     // The code that defines your stack goes here
-    const bucketName: string = process.env.THUMBING_BUCKET_NAME as string;
+    const uploadsBucketName: string = process.env.UPLOADS_BUCKET_NAME as string;
+    const assetsBucketName: string = process.env.ASSETS_BUCKET_NAME as string;
     const folderInput: string = process.env.THUMBING_S3_FOLDER_INPUT as string;
     const folderOutput: string = process.env.THUMBING_S3_FOLDER_OUTPUT as string;
     const webhookUrl: string = process.env.THUMBING_WEBHOOK_URL as string;
     const topicName: string = process.env.THUMBING_TOPIC_NAME as string;
     const functionPath: string = process.env.THUMBING_FUNCTION_PATH as string;
     
-    // const bucket = this.createBucket(bucketName);
-    // import existing bucket
-    const bucket = this.importBucket(bucketName)
+    const uploadsBucket = this.createBucket(uploadsBucketName);
+    const assetsBucket = this.importBucket(assetsBucketName) // import existing bucket
+
 
     // create lambda
-    const lambda = this.createLambda(functionPath, bucketName, folderInput, folderOutput);
+    const lambda = this.createLambda(functionPath, uploadsBucketName, assetsBucketName, folderInput, folderOutput);
     
     // create sns topic and subscription
     const snsTopic = this.createSnsTopic(topicName)
     this.createSnsSubscription(snsTopic,webhookUrl)
 
     //Add S3 event notifications
-    this.createS3NotifyToLambda(folderInput,lambda,bucket)
-    this.createS3NotifyToSns(folderOutput,snsTopic,bucket)
+    this.createS3NotifyToLambda(folderInput,lambda,uploadsBucket)
+    this.createS3NotifyToSns(folderOutput,snsTopic,assetsBucket)
     
     //Create policies
-    const s3ReadWritePolicy = this.createPolicyBucketAccess(bucket.bucketArn)
+    const s3UploadsReadWritePolicy = this.createPolicyBucketAccess(uploadsBucket.bucketArn)
+    const s3AssetsReadWritePolicy = this.createPolicyBucketAccess(assetsBucket.bucketArn)
     // const snsPublishPolicy = this.createPolicySnSPublish(snsTopic.topicArn)
     
     //Attach policies for permission
-    lambda.addToRolePolicy(s3ReadWritePolicy);
+    lambda.addToRolePolicy(s3UploadsReadWritePolicy);
+    lambda.addToRolePolicy(s3AssetsReadWritePolicy);
     // lambda.addToRolePolicy(snsPublishPolicy);
   }
 
   createBucket(bucketName: string): s3.IBucket {
-    const bucket = new s3.Bucket(this, 'ThumbingBucket', {
+    const bucket = new s3.Bucket(this, 'UploadsBucket', {
       bucketName: bucketName,
       removalPolicy: cdk.RemovalPolicy.DESTROY
     });
@@ -63,14 +66,14 @@ export class ThumbingServerlessCdkStack extends cdk.Stack {
     return bucket
   }
 
-  createLambda(functionPath:string, bucketName:string, folderInput:string, folderOutput:string): lambda.IFunction {
+  createLambda(functionPath:string, uploadsBucketName:string, assetsBucketName:string, folderInput:string, folderOutput:string): lambda.IFunction {
   
     const lambdaFunction = new lambda.Function(this, 'ThumbLambda', {
       runtime: lambda.Runtime.NODEJS_18_X,
       handler: 'index.handler',
       code: lambda.Code.fromAsset(functionPath),
       environment: {
-        DEST_BUCKET_NAME: bucketName,
+        DEST_BUCKET_NAME: assetsBucketName,
         FOLDER_INPUT: folderInput,
         FOLDER_OUTPUT: folderOutput,
         PROCESS_WIDTH: '512',
@@ -83,8 +86,8 @@ export class ThumbingServerlessCdkStack extends cdk.Stack {
   createS3NotifyToLambda(prefix: string, lambda: lambda.IFunction, bucket: s3.IBucket): void {
     const destination = new s3n.LambdaDestination(lambda);
       bucket.addEventNotification(s3.EventType.OBJECT_CREATED_PUT,
-      destination,
-      {prefix: prefix} //folder to container te original image
+      destination//,
+      //{prefix: prefix} //folder to container te original image
     )
   }
 
