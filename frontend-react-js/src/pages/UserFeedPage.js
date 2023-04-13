97f9496ef215d58eacdcc99287abc7afeba67e8b
annleefores <annleefores@gmail.com>
2023-04-13 16:20:19 +0530

    feat: edit and reply pop up

--- frontend-react-js/src/pages/UserFeedPage.js
+++ frontend-react-js/src/pages/UserFeedPage.js
@@ -2,13 +2,14 @@ import "./UserFeedPage.css";
 import React from "react";
 import { useParams } from "react-router-dom";
 
-import DesktopNavigation from "../components/DesktopNavigation";
-import DesktopSidebar from "../components/DesktopSidebar";
-import ActivityFeed from "../components/ActivityFeed";
-import ActivityForm from "../components/ActivityForm";
+import DesktopNavigation from 'components/DesktopNavigation';
+import DesktopSidebar from 'components/DesktopSidebar';
+import ActivityFeed from 'components/ActivityFeed';
+import ActivityForm from 'components/ActivityForm';
+import ProfileHeading from 'components/ProfileHeading';
+import ProfileForm from 'components/ProfileForm';
 
-import { checkAuth, getAccessToken } from "../lib/CheckAuth";
-import ProfileHeading from "../components/ProfileHeading";
+import { checkAuth, getAccessToken } from 'lib/CheckAuth';
 
 export default function UserFeedPage() {
   const [activities, setActivities] = React.useState([]);
@@ -57,6 +58,11 @@ export default function UserFeedPage() {
       <DesktopNavigation user={user} active={"profile"} setPopped={setPopped} />
       <div className="content">
         <ActivityForm popped={popped} setActivities={setActivities} />
+        <ProfileForm
+          profile={profile}
+          popped={poppedProfile}
+          setPopped={setPoppedProfile}
+        />
         <div className="activity_feed">
           <ProfileHeading setPopped={setPoppedProfile} profile={profile} />
           <ActivityFeed activities={activities} />
