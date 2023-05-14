c586c4bf7ec7cc14de5d228b9805ee10b0b16bb4
annleefores <annleefores@gmail.com>
2023-05-14 22:06:53 +0530

    db tf init

--- terraform/db/main.tf
+++ terraform/db/main.tf
@@ -1,1 +1,48 @@
+terraform {
+  backend "s3" {
+    bucket         = "tf-state-annlee"
+    key            = "terraform/db/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "tf-state-lock"
+    encrypt        = true
 
+  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+
+  required_version = "~> 1.4.6"
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+
+resource "aws_db_instance" "crd_rds_tf" {
+  allocated_storage           = 20
+  db_name                     = var.crd_db_tf
+  allow_major_version_upgrade = true
+  auto_minor_version_upgrade  = true
+  backup_retention_period     = var.backup_retention_period
+  engine                      = "postgres"
+  engine_version              = var.engine_version
+  instance_class              = var.instance_class
+  identifier_prefix           = "cruddur-db-tf"
+  username                    = var.username
+  password                    = var.password
+  deletion_protection         = var.deletion_protection
+  performance_insights_enabled = true
+  publicly_accessible         = true
+  db_subnet_group_name        = 
+  vpc_security_group_ids      = 
+
+}
+
+#DB subnet group
+
+#DB SG
---
+++ terraform/db/variables.tf
@@ -0,0 +1,30 @@
+
+variable "backup_retention_period" {
+  type    = number
+  default = 0
+}
+variable "crd_db_tf" {
+  type    = string
+  default = "crd_db_tf"
+}
+variable "deletion_protection" {
+  type    = bool
+  default = true
+}
+
+variable "engine_version" {
+  type    = string
+  default = "15.2"
+}
+variable "instance_class" {
+  type    = string
+  default = "db.t3.micro"
+}
+variable "password" {
+  type      = string
+  sensitive = true
+}
+
+variable "username" {
+  type = string
+}
