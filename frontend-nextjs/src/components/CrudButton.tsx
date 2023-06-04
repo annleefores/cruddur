2395bc9e6318d608d64bb304c64ea7c788993bdd
annleefores <annleefores@gmail.com>
2023-06-04 12:23:07 +0530

    fix: pencil icon changed to hi2 icon

--- frontend-nextjs/src/components/CrudButton.tsx
+++ frontend-nextjs/src/components/CrudButton.tsx
@@ -1,11 +1,11 @@
-import { TbEdit } from "react-icons/tb";
+import { HiOutlinePencilSquare } from "react-icons/hi2";
 const CrudButton = () => {
   return (
     <div>
-      <button className=" text-neutral-400 hover:text-white sm:text-white font-bold p-2 sm:p-3 xl:p-4 w-full rounded-full hover:bg-[#46108d] sm:bg-[#9500FF]">
+      <button className=" text-neutral-400 hover:text-white sm:text-white font-bold p-1.5 sm:p-3 xl:p-4 w-full rounded-full hover:bg-[#46108d] sm:hover:sm:bg-[#8c06ec] sm:bg-[#9500FF]">
         <p className="hidden xl:block">Crud</p>
-        <div className="block xl:hidden">
-          <TbEdit size={24} />
+        <div className="block xl:hidden rounded-full">
+          <HiOutlinePencilSquare className="w-7 h-7 sm:w-6 sm:h-6" />
         </div>
       </button>
     </div>
