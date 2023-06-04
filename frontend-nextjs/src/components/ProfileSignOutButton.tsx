b0c0fdfda316c16bb58397f9c9f443a5448de430
annleefores <annleefores@gmail.com>
2023-06-04 15:11:12 +0530

    logic to display username

--- frontend-nextjs/src/components/ProfileSignOutButton.tsx
+++ frontend-nextjs/src/components/ProfileSignOutButton.tsx
@@ -5,7 +5,7 @@ const ProfileSignOutButton = () => {
     <>
       <button className="text-white w-full rounded-full hover:bg-[#46108d] transition xl:py-3 xl:px-2">
         <div className="flex flex-row justify-between items-center gap-x-2">
-          <UserProfile />
+          <UserProfile SignOutProfile={true} />
           <div className="hidden xl:block text-neutral-400">
             <HiOutlineDotsHorizontal size={26} />
           </div>
