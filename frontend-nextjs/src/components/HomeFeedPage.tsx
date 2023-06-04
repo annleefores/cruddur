9a9305adac075b0735c2dd83797c4dbae8138841
annleefores <annleefores@gmail.com>
2023-06-04 11:38:51 +0530

    responsive navigation edit and profile

--- frontend-nextjs/src/components/HomeFeedPage.tsx
+++ frontend-nextjs/src/components/HomeFeedPage.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import logo from "../../public/logo.svg";
 import user from "../../public/user.png";
+import ProfileSignOutButton from "./ProfileSignOutButton";
 
 const HomeFeedPage = () => {
   const items = [
@@ -49,12 +50,12 @@ const HomeFeedPage = () => {
   return (
     <div className="flex flex-col h-full w-full">
       <div className="flex flex-row justify-between sticky top-0 left-0 w-full py-3 px-4 bg-black border-b border-neutral-700">
-        <h1 className="text-lg sm:text-2xl font-bold h-fit">Home</h1>
+        <h1 className="text-lg sm:text-xl font-bold h-fit">Home</h1>
         <div className="block sm:hidden w-full max-h-[32px] max-w-[32px] mb-2 rounded-full xl:mx-2">
           <Image src={logo} alt="cruddur-logo" className="object-cover" />
         </div>
-        <div className="block sm:hidden w-full max-h-[32px] max-w-[32px] mb-2 rounded-full xl:mx-2 bg-white">
-          <Image src={user} alt="user-profile" className="object-cover" />
+        <div className="block sm:hidden w-full max-h-[32px] max-w-[32px] ">
+          <ProfileSignOutButton />
         </div>
       </div>
       <div className="flex flex-col w-full overflow-y-scroll no-scrollbar">
--- frontend-nextjs/src/components/Navigation.tsx
+++ frontend-nextjs/src/components/Navigation.tsx
@@ -9,6 +9,8 @@ import {
 import Image from "next/image";
 import logo from "../../public/logo.svg";
 import NavigationItems from "./NavigationItems";
+import CrudButton from "./CrudButton";
+import ProfileSignOutButton from "./ProfileSignOutButton";
 
 const Navigation = () => {
   const username = "annleefores";
@@ -41,7 +43,7 @@ const Navigation = () => {
   ];
 
   return (
-    <div className="flex flex-row sm:flex-col justify-center sm:justify-start w-full sm:w-16 xl:w-[275px] bg-[#3D0D7B] p-2 ">
+    <div className="relative flex flex-row sm:flex-col justify-center sm:justify-start w-full sm:w-16 xl:w-[275px] bg-[#3D0D7B] p-2 ">
       <div className="hidden sm:block w-full max-h-[50px] max-w-[50px] mb-2 rounded-full xl:mx-2">
         <Image src={logo} alt="cruddur-logo" className="object-cover" />
       </div>
@@ -49,11 +51,12 @@ const Navigation = () => {
         {routes.map((item) => (
           <NavigationItems key={item.label} {...item} />
         ))}
-      </div>
-      <div>
-        <button className="hidden xl:block text-white font-bold py-2 px-4 w-44 rounded-full bg-[#9500FF]">
-          Crud
-        </button>
+        <div className="sm:mt-8 xl:p-2 fixed bottom-24 right-5 sm:static sm:bottom-auto sm:right-auto ">
+          <CrudButton />
+        </div>
+        <div className="hidden sm:block mt-6 xl:mt-2 p-1 xl:p-2">
+          <ProfileSignOutButton />
+        </div>
       </div>
     </div>
   );
