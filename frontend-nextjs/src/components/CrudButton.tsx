af2a3850fd28aac181cb13e7f9258ef9c4742d25
annleefores <annleefores@gmail.com>
2023-06-04 12:16:04 +0530

    remove edit btn bg in small screen

--- frontend-nextjs/src/components/CrudButton.tsx
+++ frontend-nextjs/src/components/CrudButton.tsx
@@ -2,7 +2,7 @@ import { TbEdit } from "react-icons/tb";
 const CrudButton = () => {
   return (
     <div>
-      <button className=" text-white font-bold p-2 sm:p-3 xl:p-4 w-full rounded-full bg-[#9500FF]">
+      <button className=" text-neutral-400 hover:text-white sm:text-white font-bold p-2 sm:p-3 xl:p-4 w-full rounded-full hover:bg-[#46108d] sm:bg-[#9500FF]">
         <p className="hidden xl:block">Crud</p>
         <div className="block xl:hidden">
           <TbEdit size={24} />
