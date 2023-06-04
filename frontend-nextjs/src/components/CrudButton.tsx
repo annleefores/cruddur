10058a932d146bf2a5b1fe1eafad5b089f2d8285
annleefores <annleefores@gmail.com>
2023-06-04 12:11:50 +0530

    floating edit button to bottom nav

--- frontend-nextjs/src/components/CrudButton.tsx
+++ frontend-nextjs/src/components/CrudButton.tsx
@@ -2,7 +2,7 @@ import { TbEdit } from "react-icons/tb";
 const CrudButton = () => {
   return (
     <div>
-      <button className=" text-white font-bold p-3 xl:p-4 w-full rounded-full bg-[#9500FF]">
+      <button className=" text-white font-bold p-2 sm:p-3 xl:p-4 w-full rounded-full bg-[#9500FF]">
         <p className="hidden xl:block">Crud</p>
         <div className="block xl:hidden">
           <TbEdit size={24} />
