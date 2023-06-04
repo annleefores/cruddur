f43ba6f736eda454f563eec89f7917049d6e1f41
annleefores <annleefores@gmail.com>
2023-06-04 12:35:37 +0530

    fix: crud button padding on xl device

--- frontend-nextjs/src/components/CrudButton.tsx
+++ frontend-nextjs/src/components/CrudButton.tsx
@@ -1,9 +1,9 @@
 import { HiOutlinePencilSquare } from "react-icons/hi2";
 const CrudButton = () => {
   return (
-    <button className=" text-neutral-400 hover:text-white sm:text-white font-bold p-1 sm:p-2 w-full rounded-full hover:bg-[#46108d] sm:hover:sm:bg-[#8c06ec] sm:bg-[#9500FF]">
+    <button className=" text-neutral-400 text-center hover:text-white sm:text-white font-bold p-1 sm:p-2 xl:py-3 w-full rounded-full hover:bg-[#46108d] sm:hover:sm:bg-[#8c06ec] sm:bg-[#9500FF]">
       <HiOutlinePencilSquare className="block xl:hidden w-7 h-7 sm:w-6 sm:h-6" />
-      <p className="hidden xl:block">Crud</p>
+      <p className="hidden xl:block ">Crud</p>
     </button>
   );
 };
