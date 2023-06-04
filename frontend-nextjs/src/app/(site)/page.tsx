a16e37d12ce0e2df84943a6b0f3b299ca4f2754e
annleefores <annleefores@gmail.com>
2023-06-04 09:47:15 +0530

    justify between homefeed and search

--- frontend-nextjs/src/app/(site)/page.tsx
+++ frontend-nextjs/src/app/(site)/page.tsx
@@ -4,13 +4,13 @@ import RightSidebar from "@/components/RightSidebar";
 export default function Home() {
   return (
     <>
-      <div className="flex flex-row justify-normal h-full w-full ">
+      <div className="flex flex-row justify-between h-full w-full ">
         <div className="bg-black max-w-[650px] h-full w-full">
           <HomeFeedPage />
         </div>
-        {/* <div className="hidden lg:block h-full max-w-[320px] px-2 py-4 mx-5 ">
+        <div className="hidden lg:block h-full p-3">
           <RightSidebar />
-        </div> */}
+        </div>
       </div>
     </>
   );
