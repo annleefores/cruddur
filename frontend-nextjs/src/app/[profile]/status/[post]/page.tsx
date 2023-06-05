8a874c756f42f6b8c8063b6604c00fd5c0d751f2
annleefores <annleefores@gmail.com>
2023-06-05 13:06:02 +0530

    crud page wip

---
+++ frontend-nextjs/src/app/[profile]/status/[post]/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import HeaderElem from "@/components/HeaderElem";
+import React from "react";
+
+const page = () => {
+  return (
+    <div className="bg-black h-full w-full">
+      <HeaderElem page={`Crud`} />
+    </div>
+  );
+};
+
+export default page;
