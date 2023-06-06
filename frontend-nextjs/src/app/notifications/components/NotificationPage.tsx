82cbdd36041ece4aa01bab23d97f5003dc9df8d0
annleefores <annleefores@gmail.com>
2023-06-06 15:30:05 +0530

    demo notification page

---
+++ frontend-nextjs/src/app/notifications/components/NotificationPage.tsx
@@ -0,0 +1,24 @@
+import { notifications } from "@/lib/data";
+import CrudNotification from "./CrudNotification";
+
+const NotificationPage = () => {
+  return (
+    <div>
+      {notifications.map((notification, index) => (
+        <div
+          key={index}
+          className=" w-full p-3 sm:w-full h-full hover:bg-neutral-900 border-b border-neutral-800 transition cursor-pointer"
+        >
+          <CrudNotification {...notification} />
+        </div>
+      ))}
+      <div className="p-4 h-full w-full border-t border-neutral-800">
+        <div className="h-[100px] text-center text-sm text-neutral-500">
+          <p>{`You're all caught up!`}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NotificationPage;
