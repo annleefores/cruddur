1259375cff846421f0c3ef2a0e05dbf156ac35ff
annleefores <annleefores@gmail.com>
2023-06-06 14:24:30 +0530

    feat: profile page with cruds

--- frontend-nextjs/src/components/HomeFeedPage.tsx
+++ frontend-nextjs/src/components/HomeFeedPage.tsx
@@ -1,10 +1,8 @@
 import Crud from "./Crud";
+import CrudPage from "./CrudPage";
 import HeaderElem from "./HeaderElem";
-import { data } from "@/lib/data";
 
 const HomeFeedPage = () => {
-  const items = data;
-
   return (
     <div className="flex flex-col h-full w-full">
       <HeaderElem page={"Home"} />
@@ -14,19 +12,7 @@ const HomeFeedPage = () => {
           <div className="block sm:hidden h-full w-full border-b border-neutral-800">
             <div className="mb-14"> </div>
           </div>
-          {items.map((item, index) => (
-            <div
-              key={index}
-              className=" w-full p-3 sm:w-full h-full hover:bg-neutral-900 border-b border-neutral-800 transition cursor-pointer"
-            >
-              <Crud {...item} />
-            </div>
-          ))}
-          <div className="p-4 h-full w-full border-t border-neutral-800">
-            <div className="h-[100px] text-center text-sm text-neutral-500">
-              <p>{`You're all caught up!`}</p>
-            </div>
-          </div>
+          <CrudPage />
         </div>
       </div>
     </div>
