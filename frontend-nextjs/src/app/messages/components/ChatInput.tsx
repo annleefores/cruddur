85f774aaae7067e40653afc1e10438e04d7c808d
annleefores <annleefores@gmail.com>
2023-06-06 22:52:09 +0530

    chat input wip

---
+++ frontend-nextjs/src/app/messages/components/ChatInput.tsx
@@ -0,0 +1,9 @@
+const ChatInput = () => {
+  return (
+    <div className=" p-2 py-3 bg-gray-800 m-2">
+      <input type="text" className="p-2 w-full bg-gray-800 outline-none" />
+    </div>
+  );
+};
+
+export default ChatInput;
--- frontend-nextjs/src/app/messages/components/ChatPage.tsx
+++ frontend-nextjs/src/app/messages/components/ChatPage.tsx
@@ -1,17 +1,22 @@
 import ChatBubble from "./ChatBubble";
 import { messages } from "@/lib/data";
+import ChatInput from "./ChatInput";
 
 const ChatPage = () => {
   return (
-    <div className="h-full">
-      {messages.map((message, index) => (
-        <ChatBubble
-          key={index}
-          text={message.text}
-          isUser={message.isUser}
-          previousIsUser={index > 0 ? messages[index - 1].isUser : false}
-        />
-      ))}
+    <div className="mt-6">
+      <div className="h-full">
+        {messages.map((message, index) => (
+          <ChatBubble
+            key={index}
+            text={message.text}
+            isUser={message.isUser}
+            previousIsUser={index > 0 ? messages[index - 1].isUser : false}
+          />
+        ))}
+      </div>
+      <div className="h-16"> </div>
+      {/* <ChatInput /> */}
     </div>
   );
 };
