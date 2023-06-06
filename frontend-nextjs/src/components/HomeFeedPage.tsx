5a7c8a461a2b009420bfb0343ff1f0e0c8fd7046
annleefores <annleefores@gmail.com>
2023-06-06 15:37:37 +0530

    notification and transparent home header

--- frontend-nextjs/src/components/HomeFeedPage.tsx
+++ frontend-nextjs/src/components/HomeFeedPage.tsx
@@ -4,10 +4,10 @@ import HeaderElem from "./HeaderElem";
 
 const HomeFeedPage = () => {
   return (
-    <div className="flex flex-col h-full w-full">
+    <div className="flex flex-col h-full w-full overflow-y-scroll no-scrollbar">
       <HeaderElem page={"Home"} />
 
-      <div className="overflow-y-scroll no-scrollbar ">
+      <div className=" ">
         <div className="flex flex-col w-full justify-center ">
           <div className="block sm:hidden h-full w-full border-b border-neutral-800">
             <div className="mb-14"> </div>
