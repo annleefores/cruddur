be33a292c830b90eda6d99cddcec64ca25ea56b8
annleefores <annleefores@gmail.com>
2023-06-07 13:18:26 +0530

    root layout split in to sub layouts

--- frontend-nextjs/src/app/(site)/page.tsx
+++ frontend-nextjs/src/app/(site)/page.tsx
@@ -4,9 +4,12 @@ import RightSidebar from "@/components/RightSidebar";
 export default function Home() {
   return (
     <>
-      <div className="flex flex-row justify-start gap-x-2 h-full w-full ">
-        <div className="bg-black h-full w-full">
+      <div className="flex flex-row justify-start gap-x-2 h-full w-full  ">
+        <div className="bg-black h-full w-full md:w-[600px] ">
           <HomeFeedPage />
+        </div>
+        <div className="hidden h-full lg:block w-[370px]">
+          <RightSidebar />
         </div>
       </div>
     </>
