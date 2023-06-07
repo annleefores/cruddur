48dc1ab8adfc066466049373fe86282c9c33ecf9
annleefores <annleefores@gmail.com>
2023-06-07 20:33:14 +0530

    feat: chat list and chat display logic

---
+++ frontend-nextjs/src/app/messages/components/MessageComponent.tsx
@@ -0,0 +1,77 @@
+"use client";
+import HeaderElem from "@/components/HeaderElem";
+import { users } from "@/lib/data";
+import Link from "next/link";
+import UserListBox from "./UserListBox";
+import React, { useEffect, useState } from "react";
+import ChatPage from "./ChatPage";
+import ChatInput from "./ChatInput";
+import { twMerge } from "tailwind-merge";
+// import { usePathname } from "next/navigation";
+// import { isChat } from "@/lib/isChat";
+
+interface MessageComponent {
+  Msg: boolean;
+  userhandle: string;
+}
+
+const MessageComponent: React.FC<MessageComponent> = ({ Msg, userhandle }) => {
+  //   const pathname = usePathname();
+  const [selectedUser, setSelectedUser] = useState("");
+
+  useEffect(() => {
+    setSelectedUser(userhandle);
+  }, [userhandle]);
+
+  return (
+    <div className="flex flex-row gap-1 h-full w-full">
+      <div
+        className={twMerge(
+          "bg-black w-screen sm:w-[360px] h-full overflow-y-scroll no-scrollbar  border-r border-neutral-800",
+          !Msg && "hidden sm:block"
+        )}
+      >
+        <HeaderElem page={"Messages"} />
+
+        <div className="h-full pt-14 sm:pt-0 ">
+          <div className="w-full">
+            {users.map((user, index) => (
+              <Link key={index} href={`/messages/new/${user.userhandle}`}>
+                <div
+                  className="p-3 py-5  h-full hover:bg-neutral-900
+          border-b border-neutral-800 transition cursor-pointer"
+                >
+                  <UserListBox {...user} />
+                </div>
+              </Link>
+            ))}
+            <div className="h-16"> </div>
+          </div>
+        </div>
+      </div>
+      <div
+        className={twMerge(
+          "hidden sm:block flex-grow overflow-y-scroll no-scrollbar bg-black",
+          !Msg && "block "
+        )}
+      >
+        {Msg ? (
+          <div className="flex flex-col items-center justify-center h-full p-4">
+            <p className="text-3xl font-bold">Select a message</p>
+            <p className="text-neutral-500">
+              Choose from your existing conversations or start a new one
+            </p>
+          </div>
+        ) : (
+          <>
+            <HeaderElem page={selectedUser} />
+            <ChatPage />
+            <ChatInput />
+          </>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default MessageComponent;
--- frontend-nextjs/src/components/HeaderElem.tsx
+++ frontend-nextjs/src/components/HeaderElem.tsx
@@ -5,9 +5,7 @@ import logo from "../../public/logo.svg";
 import ProfileSignOutButton from "./ProfileSignOutButton";
 import React from "react";
 import Link from "next/link";
-import { usePathname, useRouter } from "next/navigation";
 import { twMerge } from "tailwind-merge";
-import { isChat } from "@/lib/isChat";
 import { HiOutlineArrowLeft } from "react-icons/hi2";
 
 interface HeaderElemProps {
@@ -15,44 +13,34 @@ interface HeaderElemProps {
 }
 
 const HeaderElem: React.FC<HeaderElemProps> = ({ page }) => {
-  const username = "annleefores";
-  // eslint-disable-next-line react-hooks/rules-of-hooks
-  const router = useRouter();
-  const pathname = usePathname();
-
-  const ChatHeader = (
-    <div className="flex flex-row gap-x-4 font-bold text-lg items-center py-1">
-      <div
-        onClick={() => router.back()}
-        className="p-1 rounded-full w-fit cursor-pointer bg-neutral-800/50 hover:bg-neutral-600/50"
-      >
-        <HiOutlineArrowLeft className="w-6 h-6 sm:w-7 sm:h-7" />
-      </div>
-      <div className="inline-flex gap-x-2">
-        <div className="block sm:hidden w-full max-h-[32px] max-w-[32px] ">
-          <ProfileSignOutButton />
-        </div>
-        <div className="w-fit">
-          <p>{username}</p>
-        </div>
-      </div>
-    </div>
-  );
+  // const ChatHeader = (
+  //   <div className="flex flex-row gap-x-4 font-bold text-lg items-center py-1">
+  //     <div
+  //       onClick={() => router.back()}
+  //       className="p-1 rounded-full w-fit cursor-pointer bg-neutral-800/50 hover:bg-neutral-600/50"
+  //     >
+  //       <HiOutlineArrowLeft className="w-6 h-6 sm:w-7 sm:h-7" />
+  //     </div>
+  //     <div className="inline-flex gap-x-2">
+  //       <div className="block sm:hidden w-full max-h-[32px] max-w-[32px] ">
+  //         <ProfileSignOutButton />
+  //       </div>
+  //       <div className="w-fit">
+  //         <p>{username}</p>
+  //       </div>
+  //     </div>
+  //   </div>
+  // );
 
   return (
     <div className="fixed sm:sticky top-0 left-0 z-50 w-full py-2 px-4 bg-gray-0 bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 border-b border-neutral-800">
       {/* Displays header for all pages except chat */}
-      <div
-        className={twMerge(
-          "flex flex-row justify-between items-center",
-          isChat(pathname) === "/messages/new" && "hidden"
-        )}
-      >
+      <div className="flex flex-row justify-between items-center h-6">
         <h1 className="hidden sm:block text-lg sm:text-xl font-bold h-fit">
           {page}
         </h1>
         <Link href="/" className="block sm:hidden">
-          <div className=" w-full max-h-[32px] max-w-[32px] mb-2 rounded-full xl:mx-2">
+          <div className=" w-full max-h-[32px] max-w-[32px]  rounded-full xl:mx-2">
             <Image
               src={logo}
               alt="cruddur-logo"
@@ -68,7 +56,6 @@ const HeaderElem: React.FC<HeaderElemProps> = ({ page }) => {
           <ProfileSignOutButton />
         </div>
       </div>
-      {isChat(pathname) === "/messages/new" ? ChatHeader : <></>}
     </div>
   );
 };
