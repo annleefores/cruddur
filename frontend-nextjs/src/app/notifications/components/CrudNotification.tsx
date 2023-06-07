719217d36081809ca95fca50e97e951011a8df2c
annleefores <annleefores@gmail.com>
2023-06-07 17:32:14 +0530

    tw breakpoint update

--- frontend-nextjs/src/app/notifications/components/CrudNotification.tsx
+++ frontend-nextjs/src/app/notifications/components/CrudNotification.tsx
@@ -15,7 +15,7 @@ const CrudNotification: React.FC<CrudNotificationProps> = ({
   return (
     <div className="flex flex-row gap-x-6 items-center p-2  px-4  h-full">
       <IoNotifications size={14} className="text-[#9500FF] w-1/12" />
-      <div className="flex flex-col gap-y-2 w-11/12">
+      <div className="flex flex-row gap-x-3 items-center w-11/12">
         <div className="bg-white rounded-full max-w-[30px] max-h-[30px] h-full w-full">
           <Image src={demouser} alt="demouser" className="object-cover" />
         </div>
