a0eedf99613dd68dc64297da8cc9c3661a78d8b9
annleefores <annleefores@gmail.com>
2023-06-08 13:41:46 +0530

    resolve merge conflict

--- frontend-nextjs/src/app/[profile]/layout.tsx
+++ frontend-nextjs/src/app/[profile]/layout.tsx
@@ -6,16 +6,6 @@ export default function ProfileLayout({
   children: React.ReactNode;
 }) {
   return (
-<<<<<<< HEAD
-    <section className="h-full w-full">
-      <div className="flex flex-row gap-x-2">
-        <div className="w-full h-full md:w-[600px]">{children}</div>
-        <div className="hidden lg:block w-[370px]">
-          <RightSidebar />
-        </div>
-      </div>
-    </section>
-=======
     <>
       <div className="flex flex-row gap-x-2 w-full h-full">
         <div className="w-screen sm:w-[600px]">{children}</div>
@@ -24,6 +14,5 @@ export default function ProfileLayout({
         </div>
       </div>
     </>
->>>>>>> frontend-dev
   );
 }
