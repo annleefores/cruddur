d2c1fc19007de2b13ea683b6af08f1e54e9b1cda
annleefores <annleefores@gmail.com>
2023-06-09 08:57:06 +0530

    signin page

--- frontend-nextjs/src/app/signup/page.tsx
+++ frontend-nextjs/src/app/signup/page.tsx
@@ -5,13 +5,13 @@ import logo from "../../../public/logo.svg";
 import { useState } from "react";
 import SignupForm from "./components/SignupForm";
 export default function Home() {
-  const [input1, setInput1] = useState<string>("");
-  const [input2, setInput2] = useState<string>("");
+  // const [input1, setInput1] = useState<string>("");
+  // const [input2, setInput2] = useState<string>("");
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    // Handle form submission logic here
-  };
+  // const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  //   e.preventDefault();
+  //   // Handle form submission logic here
+  // };
 
   return (
     <>
