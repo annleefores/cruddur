f59084fa56705a25f17c0faa2986f32d8feae073
annleefores <annleefores@gmail.com>
2023-06-10 17:09:33 +0530

    aws cognito signin and signout

--- frontend-nextjs/src/hooks/useAuth.tsx
+++ frontend-nextjs/src/hooks/useAuth.tsx
@@ -1,125 +1,128 @@
-// import { Amplify, Auth } from "aws-amplify";
-// import React, { createContext, useContext, useEffect, useState } from "react";
+"use client";
+import { Amplify, Auth } from "aws-amplify";
+import React, { createContext, useContext, useEffect, useState } from "react";
 
-// Amplify.configure({
-//   AWS_PROJECT_REGION: process.env.REACT_APP_AWS_PROJECT_REGION,
-//   aws_cognito_region: process.env.REACT_APP_AWS_COGNITO_REGION,
-//   aws_user_pools_id: process.env.REACT_APP_AWS_USER_POOLS_ID,
-//   aws_user_pools_web_client_id: process.env.REACT_APP_CLIENT_ID,
-//   oauth: {
-//     domain: "annlee-cruddur.auth.us-east-1.amazoncognito.com",
-//     scope: ["email", "profile", "openid", "aws.cognito.signin.user.admin"],
-//     redirectSignIn: process.env.REACT_APP_FRONTEND_URL,
-//     redirectSignOut: process.env.REACT_APP_FRONTEND_URL,
-//     responseType: "token", // or 'token', note that REFRESH token will only be generated when the responseType is code
-//   },
+Amplify.configure({
+  AWS_PROJECT_REGION: process.env.NEXT_PUBLIC_AWS_PROJECT_REGION,
+  aws_cognito_region: process.env.NEXT_PUBLIC_AWS_COGNITO_REGION,
+  aws_user_pools_id: process.env.NEXT_PUBLIC_AWS_USER_POOLS_ID,
+  aws_user_pools_web_client_id: process.env.NEXT_PUBLIC_CLIENT_ID,
+  oauth: {
+    domain: process.env.NEXT_PUBLIC_DOMAIN,
+    scope: ["email", "profile", "openid", "aws.cognito.signin.user.admin"],
+    redirectSignIn: process.env.NEXT_PUBLIC_FRONTEND_URL,
+    redirectSignOut: process.env.NEXT_PUBLIC_FRONTEND_URL,
+    responseType: "token", // or 'token', note that REFRESH token will only be generated when the responseType is code
+  },
 
-//   Auth: {
-//     // We are not using an Identity Pool
-//     // identityPoolId: process.env.REACT_APP_IDENTITY_POOL_ID, // REQUIRED - Amazon Cognito Identity Pool ID
-//     region: process.env.REACT_APP_AWS_PROJECT_REGION, // REQUIRED - Amazon Cognito Region
-//     userPoolId: process.env.REACT_APP_AWS_USER_POOLS_ID, // OPTIONAL - Amazon Cognito User Pool ID
-//     userPoolWebClientId: process.env.REACT_APP_CLIENT_ID, // OPTIONAL - Amazon Cognito Web Client ID (26-char alphanumeric string)
-//   },
-// });
+  Auth: {
+    // We are not using an Identity Pool
+    // identityPoolId: process.env.REACT_APP_IDENTITY_POOL_ID, // REQUIRED - Amazon Cognito Identity Pool ID
+    region: process.env.NEXT_PUBLIC_AWS_PROJECT_REGION, // REQUIRED - Amazon Cognito Region
+    userPoolId: process.env.NEXT_PUBLIC_AWS_USER_POOLS_ID, // OPTIONAL - Amazon Cognito User Pool ID
+    userPoolWebClientId: process.env.NEXT_PUBLIC_CLIENT_ID, // OPTIONAL - Amazon Cognito Web Client ID (26-char alphanumeric string)
+  },
+});
 
-// interface UseAuth {
-//   isLoading: boolean;
-//   isAuthenticated: boolean;
-//   display_name: string;
-//   cognito_user_uuid: string;
-//   handle: string;
-//   signIn: (username: string, password: string) => Promise<Result>;
-//   signOut: () => void;
-// }
+interface UseAuth {
+  isLoading: boolean;
+  isAuthenticated: boolean;
+  display_name: string;
+  cognito_user_uuid: string;
+  handle: string;
+  signIn: (username: string, password: string) => Promise<Result>;
+  signOut: () => Promise<Result>;
+}
 
-// interface Result {
-//   success: boolean;
-//   message: string;
-// }
+interface Result {
+  success: boolean;
+  message: string;
+}
 
-// type Props = {
-//   children?: React.ReactNode;
-// };
+type Props = {
+  children?: React.ReactNode;
+};
 
-// const authContext = createContext({} as UseAuth);
+const authContext = createContext({} as UseAuth);
 
-// export const ProvideAuth: React.FC<Props> = ({ children }) => {
-//   const auth = useProvideAuth();
-//   return <authContext.Provider value={auth}>{children}</authContext.Provider>;
-// };
+export const ProvideAuth: React.FC<Props> = ({ children }) => {
+  const auth = useProvideAuth();
+  return <authContext.Provider value={auth}>{children}</authContext.Provider>;
+};
 
-// export const useAuth = () => {
-//   return useContext(authContext);
-// };
+export const useAuth = () => {
+  return useContext(authContext);
+};
 
-// const useProvideAuth = (): UseAuth => {
-//   const [isLoading, setIsLoading] = useState(true);
-//   const [isAuthenticated, setIsAuthenticated] = useState(false);
-//   const [display_name, setdisplay_name] = useState("");
-//   const [cognito_user_uuid, setcognito_user_uuid] = useState("");
-//   const [handle, sethandle] = useState("");
+const useProvideAuth = (): UseAuth => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [display_name, setdisplay_name] = useState("");
+  const [cognito_user_uuid, setcognito_user_uuid] = useState("");
+  const [handle, sethandle] = useState("");
 
-//   useEffect(() => {
-//     Auth.currentAuthenticatedUser()
-//       .then((result) => {
-//         console.log("currentAuthUser", result);
-//         setdisplay_name(result.attributes.name);
-//         setcognito_user_uuid(result.attributes.sub);
-//         sethandle(result.attributes.preferred_username);
-//         setIsAuthenticated(true);
-//         setIsLoading(false);
-//       })
-//       .catch(() => {
-//         setdisplay_name("");
-//         setcognito_user_uuid("");
-//         sethandle("");
-//         setIsAuthenticated(false);
-//         setIsLoading(false);
-//       });
-//   }, []);
+  useEffect(() => {
+    Auth.currentAuthenticatedUser()
+      .then((result) => {
+        console.log("currentAuthUser", result);
+        setdisplay_name(result.attributes.name);
+        setcognito_user_uuid(result.attributes.sub);
+        sethandle(result.attributes.preferred_username);
+        setIsAuthenticated(true);
+        setIsLoading(false);
+      })
+      .catch(() => {
+        setdisplay_name("");
+        setcognito_user_uuid("");
+        sethandle("");
+        setIsAuthenticated(false);
+        setIsLoading(false);
+      });
+  }, []);
 
-//   const signIn = async (username: string, password: string) => {
-//     try {
-//       const result = await Auth.signIn(username, password);
+  const signIn = async (username: string, password: string) => {
+    try {
+      setIsLoading(true);
+      const result = await Auth.signIn(username, password);
 
-//       console.log("signin", result);
-//       setdisplay_name(result.attributes.name);
-//       setcognito_user_uuid(result.attributes.sub);
-//       sethandle(result.attributes.preferred_username);
-//       setIsAuthenticated(true);
-//       return { success: true, message: "LOGIN SUCCESS" };
-//     } catch (error) {
-//       return {
-//         success: false,
-//         message: "LOGIN FAIL",
-//       };
-//     }
-//   };
+      console.log("signin", result);
+      setdisplay_name(result.attributes.name);
+      setcognito_user_uuid(result.attributes.sub);
+      sethandle(result.attributes.preferred_username);
+      setIsAuthenticated(true);
+      setIsLoading(false);
+      return { success: true, message: "LOGIN SUCCESS" };
+    } catch (error) {
+      return {
+        success: false,
+        message: "LOGIN FAIL",
+      };
+    }
+  };
 
-//   const signOut = async () => {
-//     try {
-//       await Auth.signOut({ global: true });
-//       setdisplay_name("");
-//       setcognito_user_uuid("");
-//       sethandle("");
-//       setIsAuthenticated(false);
-//       return { success: true, message: "LOGOUT SUCCESS" };
-//     } catch (error) {
-//       return {
-//         success: false,
-//         message: "LOGOUT FAIL",
-//       };
-//     }
-//   };
+  const signOut = async () => {
+    try {
+      await Auth.signOut({ global: true });
+      setdisplay_name("");
+      setcognito_user_uuid("");
+      sethandle("");
+      setIsAuthenticated(false);
+      return { success: true, message: "LOGOUT SUCCESS" };
+    } catch (error) {
+      return {
+        success: false,
+        message: "LOGOUT FAIL",
+      };
+    }
+  };
 
-//   return {
-//     isLoading,
-//     isAuthenticated,
-//     display_name,
-//     cognito_user_uuid,
-//     handle,
-//     signIn,
-//     signOut,
-//   };
-// };
+  return {
+    isLoading,
+    isAuthenticated,
+    display_name,
+    cognito_user_uuid,
+    handle,
+    signIn,
+    signOut,
+  };
+};
