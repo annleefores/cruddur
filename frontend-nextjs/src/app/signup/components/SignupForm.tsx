19ce648f55cf8fca30d976b4b3344fb5a5a38f77
annleefores <annleefores@gmail.com>
2023-06-10 09:22:56 +0530

    direct integration of aws-amplify npm not working

--- frontend-nextjs/src/app/signup/components/SignupForm.tsx
+++ frontend-nextjs/src/app/signup/components/SignupForm.tsx
@@ -1,4 +1,5 @@
 "use client";
+
 import Link from "next/link";
 import SignPageHeader from "@/components/SignPageHeader";
 import { useRouter } from "next/navigation";
@@ -6,6 +7,7 @@ import { useRouter } from "next/navigation";
 import { z } from "zod";
 import { useForm, SubmitHandler } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { Auth } from "aws-amplify";
 
 const formSchema = z.object({
   name: z.string().min(1, "Name is required"),
@@ -33,9 +35,28 @@ const SignupForm = () => {
 
   const router = useRouter();
 
-  const onSubmit: SubmitHandler<FormData> = (data) => {
+  const onSubmit: SubmitHandler<FormData> = async (data) => {
+    try {
+      const { user } = await Auth.signUp({
+        username: data.email,
+        password: data.password,
+        attributes: {
+          name: data.name,
+          email: data.email,
+          preferred_username: data.username,
+        },
+        autoSignIn: {
+          // optional - enables auto sign in after user is confirmed
+          enabled: true,
+        },
+      });
+      console.log(user);
+      router.push(`/confirm?email=${data.email}`);
+    } catch (error) {
+      console.log(error);
+      //add error toast here
+    }
     console.log(data);
-    router.push(`/confirm?email=${data.email}`);
   };
 
   return (
