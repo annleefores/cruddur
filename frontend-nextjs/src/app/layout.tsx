2d57866c3a4df2c00f108504fc1a6977bc8c49e9
annleefores <annleefores@gmail.com>
2023-06-12 17:31:38 +0530

    suspense fallback disabled

--- frontend-nextjs/src/app/layout.tsx
+++ frontend-nextjs/src/app/layout.tsx
@@ -22,19 +22,19 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${inter.className} flex justify-center w-full h-full `}>
         <ProvideAuth>
-          <Suspense fallback={<Loading />}>
-            <div className="max-w-[700px] md:max-w-[800px] lg:max-w-[1065px] xl:max-w-[1265px] flex flex-row  w-full h-full">
-              <div className="h-full">
-                <LeftSidebar />
-              </div>
-              <div className="flex-1 h-full">
-                {children}
-                <Suspense fallback={null}>
-                  <NavigationEvents />
-                </Suspense>
-              </div>
+          {/* <Suspense fallback={<Loading />}> */}
+          <div className="max-w-[700px] md:max-w-[800px] lg:max-w-[1065px] xl:max-w-[1265px] flex flex-row  w-full h-full">
+            <div className="h-full">
+              <LeftSidebar />
             </div>
-          </Suspense>
+            <div className="flex-1 h-full">
+              {children}
+              <Suspense fallback={null}>
+                <NavigationEvents />
+              </Suspense>
+            </div>
+          </div>
+          {/* </Suspense> */}
         </ProvideAuth>
       </body>
     </html>
