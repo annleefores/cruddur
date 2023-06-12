e613962f09040873831fe1f0db5c89d248473fe5
annleefores <annleefores@gmail.com>
2023-06-12 16:51:21 +0530

    loading spinner flex removed

--- frontend-nextjs/src/app/loading.tsx
+++ frontend-nextjs/src/app/loading.tsx
@@ -2,9 +2,5 @@ import LoadingSpinner from "@/components/LoadingSpinner";
 
 export default function Loading() {
   // You can add any UI inside Loading, including a Skeleton.
-  return (
-    <div className="flex  w-full h-full">
-      <LoadingSpinner />
-    </div>
-  );
+  return <LoadingSpinner />;
 }
