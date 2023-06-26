f725630e7af05c1fe302b7359f19e096d97f7159
annleefores <annleefores@gmail.com>
2023-06-26 19:50:31 +0530

    sam build docker deploy nextjs app to lambda

---
+++ terraform/frontend-nextjs/main.tf
@@ -0,0 +1,28 @@
+
+terraform {
+  backend "s3" {
+    bucket         = "tf-state-annlee"
+    key            = "terraform/frontend-nextjs/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "tf-state-lock"
+    encrypt        = true
+
+  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+    archive = {
+      source  = "hashicorp/archive"
+      version = "2.3.0"
+    }
+  }
+
+  required_version = "~> 1.4.6"
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
