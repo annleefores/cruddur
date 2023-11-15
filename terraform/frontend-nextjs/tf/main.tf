3fb326364308f5361d6804d8dcf5bfa2dd48128d
annleefores <annleefores@gmail.com>
2023-11-15 22:01:23 +0530

    add tf state

--- terraform/frontend-nextjs/tf/main.tf
+++ terraform/frontend-nextjs/tf/main.tf
@@ -1,6 +1,15 @@
 
 terraform {
 
+  backend "s3" {
+    bucket         = "tf-state-annlee"
+    key            = "terraform/frontend-nextjs/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "tf-state-lock"
+    encrypt        = true
+
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
